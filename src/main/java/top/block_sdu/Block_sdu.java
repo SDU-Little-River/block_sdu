c0b0cc0a01a27a8234dd5df08c8565f1e9f174fc
xhbsh <260342855@qq.com>
2024-08-17 13:16:12 +0800

    Initial commit

---
+++ src/main/java/top/block_sdu/Block_sdu.java
@@ -0,0 +1,11 @@
+package top.block_sdu;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Block_sdu implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/top/block_sdu/mixin/FallingBlockMixin.java
@@ -0,0 +1,24 @@
+package org.example1.fksd.mixin;
+
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Fallable;
+import net.minecraft.world.level.block.FallingBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FallingBlock.class)
+public abstract class FallingBlockMixin extends Block implements Fallable {
+    public FallingBlockMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Inject(method = "isFree",at=@At("HEAD"), cancellable = true)
+    private static void isFree(BlockState blockState, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
+    }
+    }
+
